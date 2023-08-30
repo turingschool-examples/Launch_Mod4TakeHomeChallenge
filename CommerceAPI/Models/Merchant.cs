440c2ca174926fb934df25161a5174098282da93
jkimball54 <jeremyck54@gmail.com>
2023-08-30 13:31:18 -0600

    Products property added to merchant model; Dbset<Product> added to context; migrations added

--- CommerceAPI/Models/Merchant.cs
+++ CommerceAPI/Models/Merchant.cs
@@ -3,6 +3,7 @@
     public class Merchant
     {
         public int Id { get; set; }
+        public List<Product> Products { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
     }
