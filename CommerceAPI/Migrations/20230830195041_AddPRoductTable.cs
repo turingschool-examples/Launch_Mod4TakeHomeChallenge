81fb4877fe177adf55b8070e40c7422602d4ffa3
skylarbsandler <skylarbsandler@gmail.com>
2023-08-30 13:52:24 -0600

    Product Model Created and Db Updated

---
+++ CommerceAPI/Migrations/20230830195041_AddPRoductTable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CommerceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPRoductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    category = table.Column<string>(type: "text", nullable: false),
+                    price_in_cents = table.Column<int>(type: "integer", nullable: false),
+                    stock_quantity = table.Column<int>(type: "integer", nullable: false),
+                    release_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    merchant_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_products", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_products_merchants_merchant_id",
+                        column: x => x.merchant_id,
+                        principalTable: "merchants",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_products_merchant_id",
+                table: "products",
+                column: "merchant_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+        }
+    }
+}
