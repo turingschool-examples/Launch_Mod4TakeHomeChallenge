ba62d6525e455415d52ed10e9174d86642d21bef
jkimball54 <jeremyck54@gmail.com>
2023-08-30 13:53:42 -0600

    completed ProductsController setup

--- CommerceAPI/Controllers/ProductsController.cs
+++ CommerceAPI/Controllers/ProductsController.cs
@@ -1,6 +1,18 @@
-﻿namespace CommerceAPI.Controllers
+﻿using CommerceAPI.DataAccess;
+using CommerceAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommerceAPI.Controllers
 {
+    [Route("/api/merchants/{merchantId:int}/[controller]")]
+    [ApiController]
     public class ProductsController
     {
+        private readonly CommerceApiContext _context;
+
+        public ProductsController(CommerceApiContext context)
+        {
+            _context = context;
+        }
     }
 }
