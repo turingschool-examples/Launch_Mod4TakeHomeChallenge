8f4fa79adcfa8f3498d5790e401daa3a5a4b4da6
joe10111 <centeno.joe@gmail.com>
2023-08-30 13:36:22 -0700

    Made Get All Products

---
+++ CommerceAPI/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CommerceAPI.Models;
+using CommerceAPI.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommerceAPI.Controllers
+{
+    [Route("/api/merchants/{merchantId}/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly CommerceApiContext _context;
+        public ProductsController(CommerceApiContext context)
+        {
+            _context = context;
+        }
+        
+        public ActionResult GetProducts(int merchantId)
+        {
+            var merchantWithProducts = _context.Merchants.Include(merch => merch.Products).FirstOrDefault(merch => merch.Id == merchantId);
+            
+            if (merchantWithProducts == null) 
+            {
+                return NotFound("Merchant not found");
+            }
+
+            return Ok(merchantWithProducts.Products);
+        }
+    }
+}
