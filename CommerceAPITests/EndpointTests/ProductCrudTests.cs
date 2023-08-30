67355b9302877f48ff524d919fa3af96dfba2c79
RafiWick <rafiwick@gmail.com>
2023-08-30 14:31:57 -0600

    GetProductById 404 test

--- CommerceAPITests/EndpointTests/ProductCrudTests.cs
+++ CommerceAPITests/EndpointTests/ProductCrudTests.cs
@@ -7,6 +7,7 @@ using CommerceAPI.DataAccess;
 using CommerceAPI.Models;
 using static System.Reflection.Metadata.BlobBuilder;
 using System.Text;
+using System.Net;
 
 namespace CommerceAPITests.EndpointTests
 {
@@ -75,6 +76,32 @@ namespace CommerceAPITests.EndpointTests
 
             response.EnsureSuccessStatusCode();
             Assert.Equal(expected, content);
+        }
+
+        [Fact]
+        public async void GetProductById_Returns404WithInvalidGivenId()
+        {
+            var context = GetDbContext();
+            var client = _factory.CreateClient();
+
+            var merchant1 = new Merchant { Name = "Circle K", Category = "Convenience Store" };
+            var merchant2 = new Merchant { Name = "Biker Jim's", Category = "Restaurant" };
+            var merchants = new List<Merchant> { merchant1, merchant2 };
+            context.Merchants.AddRange(merchants);
+            context.SaveChanges();
+
+            var product1 = new Product { MerchantId = merchant1.Id, Name = "Slim Jims", Description = "Meat Stick", Category = "Snack", PriceInCents = 99, StockQuantity = 100, ReleaseDate = new DateTime(2000, 1, 1, 0, 0, 0).ToUniversalTime() };
+            var product2 = new Product { MerchantId = merchant1.Id, Name = "Sweet Tarts", Description = "Sweet and Sour", Category = "Candy", PriceInCents = 149, StockQuantity = 50, ReleaseDate = new DateTime(2000, 1, 1, 0, 0, 0).ToUniversalTime() };
+            var product3 = new Product { MerchantId = merchant1.Id, Name = "Slim Jims", Description = "Meat Stick", Category = "Snack", PriceInCents = 99, StockQuantity = 100, ReleaseDate = new DateTime(2000, 1, 1, 0, 0, 0).ToUniversalTime() };
+            List<Product> products = new() { product1, product2, product3 };
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            HttpResponseMessage response = await client.GetAsync($"/api/merchants/{merchant1.Id}/products/5");
+            string content = await response.Content.ReadAsStringAsync();
+
+            var expected = HttpStatusCode.NotFound;
+            Assert.Equal(expected, response.StatusCode);
         }
 
 
