3d12fe0917bd674588b0a04914bee719d08f3163
RafiWick <rafiwick@gmail.com>
2023-08-30 14:24:49 -0600

    GetProductById test

--- CommerceAPITests/EndpointTests/ProductCrudTests.cs
+++ CommerceAPITests/EndpointTests/ProductCrudTests.cs
@@ -49,6 +49,33 @@ namespace CommerceAPITests.EndpointTests
             Assert.Equal(expected, content);
         }
 
+        [Fact]
+        public async void GetProductsById_ReturnsProductWithGivenId()
+        {
+            var context = GetDbContext();
+            var client = _factory.CreateClient();
+
+            var merchant1 = new Merchant { Name = "Circle K", Category = "Convenience Store" };
+            var merchant2 = new Merchant { Name = "Biker Jim's", Category = "Restaurant" };
+            var merchants = new List<Merchant> { merchant1, merchant2 };
+            context.Merchants.AddRange(merchants);
+            context.SaveChanges();
+
+            var product1 = new Product { MerchantId = merchant1.Id, Name = "Slim Jims", Description = "Meat Stick", Category = "Snack", PriceInCents = 99, StockQuantity = 100, ReleaseDate = new DateTime(2000, 1, 1, 0, 0, 0).ToUniversalTime() };
+            var product2 = new Product { MerchantId = merchant1.Id, Name = "Sweet Tarts", Description = "Sweet and Sour", Category = "Candy", PriceInCents = 149, StockQuantity = 50, ReleaseDate = new DateTime(2000, 1, 1, 0, 0, 0).ToUniversalTime() };
+            var product3 = new Product { MerchantId = merchant1.Id, Name = "Slim Jims", Description = "Meat Stick", Category = "Snack", PriceInCents = 99, StockQuantity = 100, ReleaseDate = new DateTime(2000, 1, 1, 0, 0, 0).ToUniversalTime() };
+            List<Product> products = new() { product1, product2, product3 };
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            HttpResponseMessage response = await client.GetAsync($"/api/merchants/{merchant1.Id}/products/{product1.Id}");
+            string content = await response.Content.ReadAsStringAsync();
+
+            string expected = ObjectToJson(product1);
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(expected, content);
+        }
 
 
 
