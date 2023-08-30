8affba04ce72ef45483ebdfa739a60fa5e1d7524
abukhmirov <alexbukhmirov@gmail.com>
2023-08-30 14:29:10 -0600

    Created CreateProduct

---
+++ CommerceAPI/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿//using CommerceAPI.DataAccess;
+//using CommerceAPI.Models;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+
+//namespace CommerceAPI.Controllers
+//{
+//    [Route("/api/[controller]")]
+//    [ApiController]
+//    public class MerchantsController : ControllerBase
+//    {
+//        private readonly CommerceApiContext _context;
+
+//        public MerchantsController(CommerceApiContext context)
+//        {
+//            _context = context;
+//        }
+
+//        [HttpGet]
+//        public ActionResult<IEnumerable<Merchant>> GetMerchants()
+//        {
+//            return _context.Merchants;
+//        }
+
+//        [HttpPost]
+//        public ActionResult CreateMerchant(Merchant merchant)
+//        {
+//            _context.Merchants.Add(merchant);
+//            _context.SaveChanges();
+
+//            return CreatedAtAction("GetMerchants", merchant);
+//        }
+//    }
+//}
