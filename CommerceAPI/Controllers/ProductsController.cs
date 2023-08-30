d871c5c7026f1f041a616d48777fe0beaa9c9cdb
bradenasmith2 <bradenasmith@icloud.com>
2023-08-30 15:13:34 -0500

    Completed action to return a single product by its primary key. this also includes migrations from the first pr that were not included but reviewed.

---
+++ CommerceAPI/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using CommerceAPI.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using CommerceAPI.Models;
+
+namespace CommerceAPI.Controllers
+{
+    [Route("/api")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly CommerceApiContext _context;
+
+        public ProductsController(CommerceApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("products/{id}")]
+        public ActionResult<Product> GetProduct_ReturnsSingleProduct(int id)
+        {
+            return _context.Products.Find(id);
+        }
+
+        [HttpGet("products")]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return _context.Products;
+        }
+    }
+}
