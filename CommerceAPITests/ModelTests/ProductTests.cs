e6086fad62c53570110d0fff13a30a6029cd58a0
jkimball54 <jeremyck54@gmail.com>
2023-08-30 13:50:46 -0600

    Finished product model testing

--- CommerceAPITests/ModelTests/ProductTests.cs
+++ CommerceAPITests/ModelTests/ProductTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommerceAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace CommerceAPITests.ModelTests
 {
     public class ProductTests
     {
+        [Fact]
+        public void IsCreatedWithNoConstructor()
+        {
+            var product = new Product { Name = "Burger", Category = "Food", Description = "Tasty", Price = 12.99M, StockQuantity = 4 };
+
+            Assert.Equal("Burger", product.Name);
+            Assert.Equal("Food", product.Category);
+            Assert.Equal("Tasty", product.Description);
+            Assert.Equal(12.99M, product.Price);
+            Assert.Equal(4, product.StockQuantity);
+
+        }
     }
 }
