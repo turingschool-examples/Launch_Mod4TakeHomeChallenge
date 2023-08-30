eaf0bc1fb4210858d53af4a26bd2f60ac184a6e1
iworsham <15iworsham@gmail.com>
2023-08-30 15:37:39 -0400

    done

--- CommerceAPI/Models/Product.cs
+++ CommerceAPI/Models/Product.cs
@@ -2,5 +2,14 @@
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public string ReleaseDate { get; set; }
+        public Merchant Merchant { get; set; }
+
     }
 }
