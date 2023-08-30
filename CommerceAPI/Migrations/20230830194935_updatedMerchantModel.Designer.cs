ae373427a1a1cfd0b56168783f58165d9cef5333
joe10111 <centeno.joe@gmail.com>
2023-08-30 12:50:27 -0700

    Updated Merchant class

--- CommerceAPI/Migrations/20230830194935_updatedMerchantModel.Designer.cs
+++ CommerceAPI/Migrations/20230830194935_updatedMerchantModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CommerceAPI.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CommerceAPI.Migrations
 {
     [DbContext(typeof(CommerceApiContext))]
-    partial class CommerceApiContextModelSnapshot : ModelSnapshot
+    [Migration("20230830194935_updatedMerchantModel")]
+    partial class updatedMerchantModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
