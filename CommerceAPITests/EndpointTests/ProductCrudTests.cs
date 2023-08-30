358e1c83f292d666aff020303d63c16737e1b04c
jkimball54 <jeremyck54@gmail.com>
2023-08-30 13:51:50 -0600

    Completed ProductCrudTests Setup

--- CommerceAPITests/EndpointTests/ProductCrudTests.cs
+++ CommerceAPITests/EndpointTests/ProductCrudTests.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using CommerceAPI.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using CommerceAPI.Models;
 
 namespace CommerceAPITests.EndpointTests
 {
     public class ProductCrudTests
     {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public ProductCrudTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+
+
+
+
+
+
+
+        private CommerceApiContext GetDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CommerceApiContext>();
+            optionsBuilder.UseInMemoryDatabase("TestDatabase");
+
+            var context = new CommerceApiContext(optionsBuilder.Options);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+
+        private string ObjectToJson(object obj)
+        {
+            DefaultContractResolver contractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new SnakeCaseNamingStrategy()
+            };
+
+            string json = JsonConvert.SerializeObject(obj, new JsonSerializerSettings
+            {
+                ContractResolver = contractResolver
+            });
+
+            return json;
+        }
     }
 }
