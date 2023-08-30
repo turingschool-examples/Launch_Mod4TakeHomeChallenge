88118f613b85757e2753c8b67193aac72b8782ae
iworsham <15iworsham@gmail.com>
2023-08-30 16:39:36 -0400

    changes

---
+++ CommerceAPI/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using CommerceAPI.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommerceAPI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly CommerceApiContext _context;
+
+        public ProductsController(CommerceApiContext context)
+        {
+            _context = context;
+        }
+       
+    }
+}
