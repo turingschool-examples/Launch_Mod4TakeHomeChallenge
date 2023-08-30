eabb32ab90acb96fd17da34668381fbc2e93f3fb
RafiWick <rafiwick@gmail.com>
2023-08-30 13:47:43 -0600

    controller and endpoint test files made

---
+++ CommerceAPI/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using CommerceAPI.DataAccess;
+using CommerceAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommerceAPI.Controllers
+{
+    [Route("/api/merchants/{merchantId:int}/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly CommerceApiContext _context;
+
+        public ProductsController(CommerceApiContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
---
+++ CommerceAPITests/EndpointTests/ProductCrudTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using CommerceAPI.DataAccess;
+using CommerceAPI.Models;
+using static System.Reflection.Metadata.BlobBuilder;
+using System.Text;
+
+namespace CommerceAPITests.EndpointTests
+{
+    public class ProductCrudTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public ProductCrudTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        
+        private CommerceApiContext GetDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CommerceApiContext>();
+            optionsBuilder.UseInMemoryDatabase("TestDatabase");
+
+            var context = new CommerceApiContext(optionsBuilder.Options);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+
+        private string ObjectToJson(object obj)
+        {
+            DefaultContractResolver contractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new SnakeCaseNamingStrategy()
+            };
+
+            string json = JsonConvert.SerializeObject(obj, new JsonSerializerSettings
+            {
+                ContractResolver = contractResolver
+            });
+
+            return json;
+        }
+    }
+}
