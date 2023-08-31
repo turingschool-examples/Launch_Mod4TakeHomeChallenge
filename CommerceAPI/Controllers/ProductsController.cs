683e51b684870a7bb69833e950c2748564f5c6b4
skylarbsandler <skylarbsandler@gmail.com>
2023-08-31 11:39:46 -0600

    CRUD Actions and Tests

--- CommerceAPI/Controllers/ProductsController.cs
+++ CommerceAPI/Controllers/ProductsController.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using CommerceAPI.DataAccess;
 using CommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommerceAPI.Controllers
 {
-    [Route("/api/merchants/{merchantId:int}/[controller]")]
+    [Route("/api/merchants/{merchantId:int}/[controller]")] //base URL
     [ApiController]
     public class ProductsController : ControllerBase
     {
@@ -14,7 +15,7 @@ namespace CommerceAPI.Controllers
             _context = context;
         }
 
-        [HttpGet]
+        [HttpGet] //Always need to indicate what HTTP method this action responds to
         public ActionResult GetProducts(int merchantId)
         {
             var merchant = _context.Merchants.FirstOrDefault(merchant => merchant.Id == merchant.Id);
@@ -41,6 +42,66 @@ namespace CommerceAPI.Controllers
                 return NotFound();
             }
             return new JsonResult(product);
+        }
+
+        [HttpPost]
+        public ActionResult CreateProduct(int merchantId, Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                //Response.StatusCode = 400;
+                return BadRequest();
+            }
+
+            var merchant = _context.Merchants.FirstOrDefault(merchant => merchant.Id == merchant.Id);
+            merchant.Products.Add(product);
+            _context.Products.Add(product);
+            _context.SaveChanges(); 
+
+            return new JsonResult(product);
+        }
+
+        [HttpPut("{productId}")]
+        public ActionResult UpdateProduct(int merchantId, int productId, Product product)
+        {
+            var merchant = _context.Merchants.Include(merchant => merchant.Products).FirstOrDefault(merchant => merchant.Id == merchantId);
+
+            if (!ModelState.IsValid)
+            {
+                Response.StatusCode = 400;
+            }
+
+            var existingProduct = merchant.Products.FirstOrDefault(product => product.Id == productId);
+            existingProduct.Name = product.Name;
+            existingProduct.Description = product.Description;
+            existingProduct.Category = product.Category;
+            existingProduct.PriceInCents = product.PriceInCents;
+            existingProduct.StockQuantity = product.StockQuantity;
+            existingProduct.ReleaseDate = product.ReleaseDate;
+
+            _context.Products.Update(existingProduct);
+            _context.SaveChanges();
+
+            Response.StatusCode = 204;
+            return new JsonResult(existingProduct); ;
+        }
+                
+        [HttpDelete("{productId}")]
+        public ActionResult DeleteProductfromId(int merchantId, int productId)
+        {
+            var merchant = _context.Merchants.FirstOrDefault(merchant => merchant.Id == merchant.Id);
+
+            if (merchant is null)
+            {
+                return NotFound();
+            }
+
+            var products = _context.Products.Where(product => product.MerchantId == merchantId);
+            var product = products.FirstOrDefault(product => product.Id == productId);
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
---
+++ CommerceAPITests/ModelTests/ProductTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommerceAPI.Models;
+
+namespace CommerceAPITests.ModelTests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void IsCreatedWithNoConstructor()
+        {
+            var product = new Product { Name = "Hamburger", Category = "Food", PriceInCents = 1000, StockQuantity = 10 };
+
+            Assert.Equal("Hamburger", product.Name);
+            Assert.Equal("Food", product.Category);
+            Assert.Equal(1000, product.PriceInCents);
+            Assert.Equal(10, product.StockQuantity);
+        }
+    }
+}
