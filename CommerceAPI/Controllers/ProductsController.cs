0b7e22b88720c1a08547f57a103241f0a9ea77e3
iworsham <15iworsham@gmail.com>
2023-08-31 13:48:38 -0400

    changes

--- CommerceAPI/Controllers/ProductsController.cs
+++ CommerceAPI/Controllers/ProductsController.cs
@@ -1,9 +1,12 @@
 ﻿using CommerceAPI.DataAccess;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using CommerceAPI.Models;
 
 namespace CommerceAPI.Controllers
 {
+    [Route("api/merchants/{merchantId/[controller]")]
+    [ApiController]
     public class ProductsController : Controller
     {
         private readonly CommerceApiContext _context;
@@ -15,14 +18,80 @@ namespace CommerceAPI.Controllers
         [HttpGet]
         public ActionResult GetProducts(int merchantId)
         {
-            var merchantProducts = _context.Merchants.Include(m => m.Products).FirstOrDefault(m => m.Id == merchantId);
+            var merchant = _context.Merchants.Include(m => m.Products).FirstOrDefault(m => m.Id == merchantId);
 
-            if (merchantProducts == null)
+            if (merchant == null)
             {
                 return NotFound("Product not found");
             }
 
-            return Ok(merchantProducts.Products);
+            return Ok(merchant.Products);
+        }
+        [HttpGet("{productId}")]
+        public ActionResult GetProductsFromId(int merchantId, int productId)
+        {
+            var merchant = _context.Merchants.FirstOrDefault(m => m.Id == merchantId);
+            if (merchant == null)
+            {
+                return NotFound(" not found");
+            }
+
+            var productsOfMerchant = merchant.Products.FirstOrDefault(p => p.Id == productId);
+            if (productsOfMerchant == null)
+            {
+                return NotFound("We do not have this item");
+            }
+
+            return new JsonResult(productsOfMerchant);
+        }
+        [HttpPut("{productId}")]
+        public ActionResult UpdateProduct(int merchantId, int productId, Product product)
+        {
+            var merchantsProducts = _context.Merchants.Include(m => m.Products).FirstOrDefault(m => m.Id == merchantId);
+            if (merchantsProducts == null)
+            {
+                return NotFound("Product not found");
+            }
+
+            var productOfMer = merchantsProducts.Products.FirstOrDefault(p => p.Id == productId);
+            if (productOfMer == null)
+            {
+                return NotFound("We do not have this item");
+            }
+
+            productOfMer.Name = product.Name;
+            productOfMer.Description = product.Description;
+            productOfMer.Category = product.Category;
+            productOfMer.MerchantId = merchantId;
+            productOfMer.Price = product.Price;
+            productOfMer.StockQuantity = product.StockQuantity;
+            productOfMer.ReleaseDate = product.ReleaseDate;
+
+
+            _context.Products.Update(productOfMer);
+            _context.SaveChanges();
+
+            return Ok(productOfMer);
+        }
+        [HttpDelete("{productId}")]
+        public ActionResult DeleteProduct(int merchantId, int productId)
+        {
+            var merchantsProducts = _context.Merchants.Include(m => m.Products).FirstOrDefault(m => m.Id == merchantId);
+            if (merchantsProducts == null)
+            {
+                return NotFound("Product not found");
+            }
+
+            var productOfMer = merchantsProducts.Products.FirstOrDefault(p => p.Id == productId);
+            if (productOfMer == null)
+            {
+                return NotFound("We do not have this item");
+            }
+
+            _context.Products.Remove(productOfMer);
+            _context.SaveChanges();
+
+            return Ok(productOfMer);
         }
     }
 }
--- CommerceAPITests/EndpointTests/ProductCrudTests.cs
+++ CommerceAPITests/EndpointTests/ProductCrudTests.cs
@@ -61,6 +61,58 @@ namespace CommerceAPITests.EndpointTests
             var expected = ObjectToJson(new List<Product> { product1, product2 });
            Assert.Contains("Coffee Maker",expected);
         }
+        [Fact]
+        public async void GetProduct_ReturnsProductFromId()
+        {
+            var context = GetDbContext();
+            // Arrange
+            Merchant merchant = new Merchant { Name = "McDonalds", Category = "Restaurant" };
+            var product1 = new Product
+            {
+                Id = 1,
+                Name = "Coffee Maker",
+                Description = "Brews up to 12 cups then breaks",
+                Category = "Home Appliances",
+                Price = 1100,
+                StockQuantity = 20,
+                ReleaseDate = "03/05/2023",
+                MerchantId = 1
+            };
+
+            var product2 = new Product
+            {
+                Id = 2,
+                Name = "Electric Guitar",
+                Description = "Solid Guitar",
+                Category = "Musical Instruments",
+                Price = 600,
+                StockQuantity = 5,
+                ReleaseDate = "05/25/2023",
+                MerchantId = 1
+            };
+
+            List<Product> products = new() { product1, product2 };
+
+            
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            HttpClient client = _factory.CreateClient();
+
+            // Act
+            HttpResponseMessage response = await client.GetAsync($"api/merchants/{merchant.Id}/products/{product1.Id}");
+            string content = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            string expected = ObjectToJson(product1);
+
+            context.ChangeTracker.Clear();
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(expected, content);
+            Assert.Contains("Coffee Maker", expected);
+            Assert.Contains("Home Appliances", expected);
+        }
 
 
 
