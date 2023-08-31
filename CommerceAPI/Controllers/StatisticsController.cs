90233504d1ea740a9e935127ccfdbc9673bffb5b
RafiWick <rafiwick@gmail.com>
2023-08-31 10:21:52 -0600

    controller actions

---
+++ CommerceAPI/Controllers/StatisticsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommerceAPI.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
